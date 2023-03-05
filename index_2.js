7b9cb58f4e09f923c26d8bdc8b5b27650f32f811
nyuzuru <yuzuru9874@gmail.com>
2023-03-05 23:00:32 +0900

    3回目

--- index_2.js
+++ index_2.js
@@ -3,3 +3,11 @@ const foo ={
     alert() {}  //短縮記法
 }
 
+function cut (food) {
+    // 切る処理...
+    const cutFood = food.slice(); //sliceは切る処理を表すために今回は使用しているため、深い意味はない。
+    return cutFood; //returnをつけないと、切っただけで結果が返ってこないため、returnをつける必要がある。
+}
+
+const cutCarrot = cut(carrot); //人参
+const cutPotato = cut(potato); //じゃがいも
