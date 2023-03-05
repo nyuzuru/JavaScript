26d5c7ba3690ebc93813f36c9eaec17857c6f147
nyuzuru <yuzuru9874@gmail.com>
2023-03-06 08:35:46 +0900

    高階関数

--- callBack.js
+++ callBack.js
@@ -3,3 +3,42 @@
 // function 高階関数（コールバック関数） {
 //     コールバック関数（）；
 // }
+
+function bring(food) {
+    if (/*手洗いが終えたら*/){
+  //   食材を持ってきてもらう処理
+    }
+  }
+  
+  function bring(food) {
+    if (/*手洗いが終えたら*/){
+  //   食材の皮を剥く処理
+    }
+  }
+  
+  function bring(food) {
+    if (/*手洗いが終えたら*/){
+  //   食材を切ってもらう処理
+    }
+  }
+  
+  
+  
+  // 高階関数を使った場合の処理
+  function washed (fn) {
+    if (/*手洗いが終えたら*/) {
+      fn(); //コール爆関数の処理
+    }
+  }
+  
+  function bring(food) {
+  //   食材を持ってきてもらう処理
+  }
+  
+  function peer(food) {
+  //   食材の皮を剥く処理
+  }
+  
+  function cut(food) {
+  //   食材を切ってもらう処理
+  }
