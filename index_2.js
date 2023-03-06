9ed7cdba57f8a9a36a1256154944a1fc33b7f144
nyuzuru <yuzuru9874@gmail.com>
2023-03-06 09:51:44 +0900

    編集

--- index_2.js
+++ index_2.js
@@ -1,3 +1,27 @@
+let youtuber = {
+    list: {
+        business: {
+            youtuber: [
+                { name: "しまぶー", age: 20, teachProgramming() {} },
+                { name: "あっちゃん", age: 37, teachHistory() {} }
+            ],
+            teach() {}
+        },
+        entertainment: {
+            youtuber: [{name: "ヒカキン"}, {name: "はじめしゃちょー"}],
+            makeSmile() {}
+        }
+    },
+    plan() {},
+    uploadVideo() {}
+};
+// console.log();
+
+// window.alert(); // window.は省略可能
+
+// window.document.getElementById()
+
+
 const foo ={
     // alert: function () {},　　短縮記法じゃない方
     alert() {}  //短縮記法
