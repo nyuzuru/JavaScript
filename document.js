29ec97b6806ca203541e00356aaa08d788b915dc
nyuzuru <yuzuru9874@gmail.com>
2023-03-06 09:08:27 +0900

    document

---
+++ document.js
@@ -0,0 +1,9 @@
+// const btn = document.getElementById("button");
+// btn.addEventListener("click", function(){
+//     console.log("フォロー解除");
+// });
+
+const foods = ["人参", "じゃがいも", "たまねぎ"];
+foods.forEach(function(food) {
+    console.log(food);
+});
